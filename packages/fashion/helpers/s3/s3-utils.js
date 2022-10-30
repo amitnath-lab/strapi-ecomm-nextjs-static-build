762ec9f526873b8ae016335d22889cc0457ea1e1
commerce essentials <amit_knath@live.com>
2022-10-30 01:41:56 +0100

    update s3-utils.js

--- packages/fashion/helpers/s3/s3-utils.js
+++ packages/fashion/helpers/s3/s3-utils.js
@@ -4,8 +4,8 @@ export async function readS3Object (bucketName, bucketKey) {
     const s3Client = new S3Client({
         region: "us-east-1",
         credentials: {
-            accessKeyId: "AKIAUV7RFCCQM2QQGC3H",
-            secretAccessKey: "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"
+            accessKeyId: "<accessKeyId>",
+            secretAccessKey: "<secretAccessKey>"
         }})
     return await s3Client.send(new GetObjectCommand({
       Bucket: bucketName,
